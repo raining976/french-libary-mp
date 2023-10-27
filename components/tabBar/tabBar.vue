8c7cbe740ef5dcb10c14a15aa8a7950703326009
raining <running.zhang@foxmail.com>
2023-10-27 23:50:49 +0800

    feat：二次封装tabbar组件

---
+++ components/tabBar/tabBar.vue
@@ -0,0 +1,49 @@
+<template>
+	<view>
+		<u-tabbar v-model='current' :safeAreaInsetBottom="true" :fixed="true" :placeholder="true" @change="changeBar">
+			<u-tabbar-item :text="tab.text" :icon="tab.iconPath" v-for="(tab, index) in tabList"
+				:key="index"></u-tabbar-item>
+		</u-tabbar>
+	</view>
+</template>
+
+<script>
+	export default {
+		name: "tabBar",
+		props:{
+			current:Number
+		},
+		data() {
+			return {
+				tabList: [{
+						"text": "首页",
+						"iconPath": "home",
+						"pagePath": "pages/index/index"
+					},
+					{
+						"text": "帮助",
+						"iconPath": "question-circle",
+						"pagePath": "pages/help/help"
+					},
+					{
+						"text": "我的",
+						"iconPath": "account",
+						"pagePath": "pages/user/user"
+					}
+				],
+			};
+		},
+		methods: {
+			changeBar(e) {
+				uni.switchTab({
+					url: '/' + this.tabList[e].pagePath
+				})
+			}
+		}
+
+	}
+</script>
+
+<style>
+
+</style>
