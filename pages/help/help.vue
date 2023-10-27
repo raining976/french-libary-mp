5a1490c6e8f15d065c262ccca0f3af56f9b0a678
raining <running.zhang@foxmail.com>
2023-10-27 23:51:09 +0800

    feat：帮助和我的页初始化

---
+++ pages/help/help.vue
@@ -0,0 +1,23 @@
+<template>
+	<view>
+		帮助页面
+		<tab-bar :current='1'></tab-bar>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
---
+++ pages/user/user.vue
@@ -0,0 +1,24 @@
+<template>
+	<view>
+		我的
+		<tab-bar :current='2'></tab-bar>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			};
+		},
+		methods: {
+
+		}
+
+	}
+</script>
+
+<style lang="scss">
+
+</style>
